bb1033566853d913ff515b157ee81cd27d21b36f
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-12-01 15:08:38 +0100

    Fixed cors typo

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from "express";
 import { createServer } from "node:http";
 import { apiRouter } from "./routes/routerHandler.js";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
+
+// cors setup
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 // parsing json bodies
 app.use(express.json());
